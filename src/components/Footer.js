0998ec2e72af4b8dfa8980019423f0d96846ef65
Erich Hartnauer <erich.hartnauer@gmail.com>
2022-03-14 18:30:15 -0500

    fixed issues

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -28,7 +28,14 @@ class Footer extends Component {
                   )
                 }
                 style={{ display: 'inline' }}
-              ></div>
+              >
+                <span
+                  className='iconify language-icon mr-5'
+                  data-icon='twemoji-flag-for-flag-united-kingdom'
+                  data-inline='false'
+                  id={window.$primaryLanguageIconId}
+                ></span>
+              </div>
               <div
                 onClick={() =>
                   this.props.applyPickedLanguage(
@@ -37,7 +44,14 @@ class Footer extends Component {
                   )
                 }
                 style={{ display: 'inline' }}
-              ></div>
+              >
+                <span
+                  className='iconify language-icon'
+                  data-icon='twemoji-flag-for-flag-poland'
+                  data-inline='false'
+                  id={window.$secondaryLanguageIconId}
+                ></span>
+              </div>
             </div>
             <div className='col'>
               <div className='copyright py-4 text-center'>
