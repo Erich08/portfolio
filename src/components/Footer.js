48024bddff39547c94d82a5674ae077cd43a8d93
Erich Hartnauer <erich.hartnauer@gmail.com>
2022-03-14 18:24:25 -0500

    added projects pics and descriptions

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,12 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
 class Footer extends Component {
   render() {
     if (this.props.sharedBasicInfo) {
       var networks = this.props.sharedBasicInfo.social.map(function (network) {
         return (
-          <span key={network.name} className="m-4">
-            <a href={network.url} target="_blank" rel="noopener noreferrer">
+          <span key={network.name} className='m-4'>
+            <a href={network.url} target='_blank' rel='noopener noreferrer'>
               <i className={network.class}></i>
             </a>
           </span>
@@ -16,51 +16,37 @@ class Footer extends Component {
 
     return (
       <footer>
-        <div className="container">
-          <div className="row">
-            <div className="col social-links">{networks}</div>
-            <div className="col">
-              <div 
+        <div className='container'>
+          <div className='row'>
+            <div className='col social-links'>{networks}</div>
+            <div className='col'>
+              <div
                 onClick={() =>
                   this.props.applyPickedLanguage(
                     window.$primaryLanguage,
                     window.$secondaryLanguageIconId
                   )
                 }
-                style={{ display: "inline" }}
-              >
-                <span
-                  className="iconify language-icon mr-5"
-                  data-icon="twemoji-flag-for-flag-united-kingdom"
-                  data-inline="false"
-                  id={window.$primaryLanguageIconId}
-                ></span>
-              </div>
-              <div 
+                style={{ display: 'inline' }}
+              ></div>
+              <div
                 onClick={() =>
                   this.props.applyPickedLanguage(
                     window.$secondaryLanguage,
                     window.$primaryLanguageIconId
                   )
                 }
-                style={{ display: "inline" }}
-              >
-                <span
-                  className="iconify language-icon"
-                  data-icon="twemoji-flag-for-flag-poland"
-                  data-inline="false"
-                  id={window.$secondaryLanguageIconId}
-                ></span>
-              </div>
+                style={{ display: 'inline' }}
+              ></div>
             </div>
-            <div className="col">
-              <div className="copyright py-4 text-center">
-                <div className="container">
+            <div className='col'>
+              <div className='copyright py-4 text-center'>
+                <div className='container'>
                   <small>
-                    Copyright &copy;{" "}
+                    Copyright &copy;{' '}
                     {this.props.sharedBasicInfo
                       ? this.props.sharedBasicInfo.name
-                      : "???"}
+                      : '???'}
                   </small>
                 </div>
               </div>
